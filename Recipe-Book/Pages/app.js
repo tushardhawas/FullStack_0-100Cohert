c10a83ede14773cb2fb68dc2a0ed3b20659b1504
tushardhawas <tushardhawasssd@gmail.com>
2024-02-06 23:08:48 +0530

    day2

--- Recipe-Book/Pages/app.js
+++ Recipe-Book/Pages/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const jwt = require("jsonwebtoken");
 const port= 3000;
+var bodyParser = require('body-parser');
 
 mongoose.connect("mongodb+srv://kirtsugue:REKxnjunOknu1N8D@cluster0.pzy0u14.mongodb.net/Recipes");
 
@@ -13,10 +14,12 @@ const recipe_data = mongoose.model("recipe_data", {
 
   const app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.post("/save",async (req,res)=>{
  const {recipeName , recipeDetails}= req.body; 
-
+console.log(recipeName);
+console.log(recipeDetails);
  const newRecipe = new recipe_data({
   
     Recipe_name: recipeName,
@@ -27,6 +30,15 @@ const savedRecipe = await newRecipe.save();
 
 res.status(201).json(savedRecipe);
 
+app.get("/recipes", async (req, res) => {
+  try {
+      const recipes = await recipe_data.find();
+      res.json(recipes);
+  } catch (error) {
+      console.error('Error fetching recipes:', error);
+      res.status(500).send('Internal Server Error');
+  }
+});
 
 })
 app.listen(port);
